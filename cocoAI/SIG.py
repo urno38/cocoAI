7b182c1fae1594018557a66acafe70a1edcf030a
lvolat <ludovic.volat@gmail.com>
2025-02-21 10:46:16 +0000

    Feb 21, 2025, 11:46 AM

--- cocoAI/SIG.py
+++ cocoAI/SIG.py
@@ -18,17 +18,18 @@ def define_formats():
     return formats_dict
 
 
-def get_unique_id_in_df(dfd, compte):
-    series_du_label = dfd.query("Compte==@compte")["Intitulé"].drop_duplicates()
+def get_unique_id_in_df(df, compte):
+    series_du_label = df.query("Compte==@compte")["Intitulé"].drop_duplicates()
     if len(series_du_label) == 1:
-        return series_du_label.loc[:, "Intitulé"]
+        print(series_du_label)
+        return str(series_du_label.loc["Intitulé"]).strip()
     elif len(series_du_label) > 1:
         LOGGER.debug(
             f"plusieurs labels pour le Compte {compte}, je prends l ID du compte"
         )
         return str(compte).strip()
-
-    return label
+    else:
+        raise ValueError(f"pas d ecriture comptable pour le compte {compte}")
 
 
 def Solde_intermediaire_de_gestion(
@@ -95,7 +96,7 @@ def Solde_intermediaire_de_gestion(
 
     for compte in compte_productions_vendues:
         row, col = add_line_SIG(
-            f"{compte} {get_unique_id_in_df(dfd,compte)}",
+            f"{compte} {get_unique_id_in_df(dfd[int(curyear)],compte)}",
             dfd[int(curyear)].query("Compte==@compte")["Débit"].sum(),
             dfd[int(refyear)].query("Compte==@compte")["Débit"].sum(),
             normal,
