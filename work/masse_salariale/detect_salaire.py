b29240c68f9e918f1d4362a855467d05cf4cda30
lvolat <ludovic.volat@gmail.com>
2025-04-25 09:41:23 +0000

    Apr 25, 2025, 11:41 AM

--- work/masse_salariale/detect_salaire.py
+++ work/masse_salariale/detect_salaire.py
@@ -1,3 +1,5 @@
+import shutil
+
 from cocoAI.folder_tree import get_enseigne_folder
 from common.identifiers import convert_to_siren
 from common.path import DATALAKE_PATH
@@ -7,5 +9,9 @@ siren = convert_to_siren(siret)
 ENSEIGNE_FOLDER = get_enseigne_folder(siret)
 
 
-for bull in list(DATALAKE_PATH.rglob("plans*")):
-    print(bull)
+# for bull in list(DATALAKE_PATH.rglob("plans*")):
+#     try:
+#         shutil.rmtree(bull)
+#         print(bull)
+#     except:
+#         pass
